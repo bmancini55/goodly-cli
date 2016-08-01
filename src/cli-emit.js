433e4991fbcfb27dc4710cb81d249ed15333407a
Brian Mancini <bmancini@gmail.com>
2016-08-01 07:46:54 -0400

    Support JSON serialization in data

--- src/cli-emit.js
+++ src/cli-emit.js
@@ -11,8 +11,16 @@ export default async function emit(options) {
 
   const service = goodly({ name: 'cli' });
   await service.start({ brokerPath: host });
-  await service.emit(event, data);
-  setTimeout(() => service.stop(), 100);
-
+  await service.emit(event, convertData(data));
+  setTimeout(() => service.stop(), 250);
   console.log('Event %s has been emitted', event);
+}
+
+function convertData(data) {
+  try {
+    return JSON.parse(data);
+  }
+  catch(ex) {
+    return data;
+  }
 }
